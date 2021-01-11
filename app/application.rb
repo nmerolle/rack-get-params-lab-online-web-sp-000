2868b69bf1beb9cb738745161d9f308132715446
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-11 02:33:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,17 +1,17 @@
 class Application
 
-  @@items = ["Apples","Carrots","Pears"]
-  @@cart = []
 
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+  @@items = ["Apples","Carrots","Pears"]	  @@items = ["Apples","Carrots","Pears"]
+	@@cart = []
 
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-      			elsif req.path.match(/cart/)
+
+  def call(env)	  def call(env)
+    resp = Rack::Response.new	    resp = Rack::Response.new
+@@ -10,6 +11,22 @@ def call(env)
+      @@items.each do |item|	      @@items.each do |item|
+        resp.write "#{item}\n"	        resp.write "#{item}\n"
+      end	      end
+		elsif req.path.match(/cart/)
 			if @@cart.empty?
 				resp.write "Your cart is empty"
 			else
@@ -27,21 +27,6 @@ class Application
 			else
 				resp.write "We don't have that item"
 			end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
-    end
-
-    resp.finish
-  end
-
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
-end
+    elsif req.path.match(/search/)	    elsif req.path.match(/search/)
+      search_term = req.params["q"]	      search_term = req.params["q"]
+      resp.write handle_search(search_term)	      resp.write handle_search(search_term)
